e7e527205fbe453b19eeff4e84bd56a0b4928014
tkdgus1397 <parksh@humanf.co.kr>
2021-05-21 15:04:00 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [category, setCategory] = useState('all');
+    const onSelect = useCallback(category => setCategory(category), []);
+
+
+
+    return (
+        <>
+            <Categories category={category} onSelect={onSelect} />
+            <NewsList category={category} />
+        </>
+    );
+};
+
 
 export default App;
